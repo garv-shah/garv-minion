938e32af34194464d90125768f0e4b6d641e20c1
Garv Shah <gshah.6110@gmail.com>
2022-03-15 20:17:16 +1100

    added timestamp to bottom

--- asx.py
+++ asx.py
@@ -1,9 +1,10 @@
-from time import sleep, time
-
-from tradingview_ta import TA_Handler, Interval, Exchange
 import csv
+from datetime import datetime
+
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib.offsetbox import AnchoredText
+from tradingview_ta import TA_Handler, Interval
 
 symbol_list = [
     "360",
@@ -358,49 +359,43 @@ symbol_list = [
     "ZIM"
 ]
 
+output_list = []
+output_dict = {}
 
-def main():
-    output_list = []
-    output_dict = {}
+for symbol in symbol_list:
+    print(symbol)
+    output = TA_Handler(
+        symbol=symbol,
+        screener="australia",
+        exchange="ASX",
+        interval=Interval.INTERVAL_1_DAY
+    )
 
-    for symbol in symbol_list:
-        print(symbol)
-        output = TA_Handler(
-            symbol=symbol,
-            screener="australia",
-            exchange="ASX",
-            interval=Interval.INTERVAL_1_DAY
-        )
+    summary = output.get_analysis().summary
 
-        summary = output.get_analysis().summary
+    output_list.append([symbol, summary])
+    if summary['RECOMMENDATION'] == 'STRONG_BUY':
+        output_dict[symbol] = summary
 
-        output_list.append([symbol, summary])
-        if summary['RECOMMENDATION'] == 'STRONG_BUY':
-            output_dict[symbol] = summary
+print(output_dict)
 
-    print(output_dict)
+with open("./data/ASX_Stock_Game_Data.csv", 'w+', newline='') as csvfile:
+    fieldnames = list(output_dict.keys())
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow(output_dict)
+    csvfile.close()
 
-    with open("./data/ASX_Stock_Game_Data.csv", 'w+', newline='') as csvfile:
-        fieldnames = list(output_dict.keys())
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerow(output_dict)
-        csvfile.close()
-
-    keys = list(output_dict.keys())
-    vals = [float(output_dict[k]['BUY']) for k in keys]
-    plt.style.use('seaborn-dark')
-    plt.figure(figsize=(18, 5))
-    sns.barplot(x=keys, y=vals)
-    plt.xlabel('\nSymbol')
-    plt.ylabel('Buy Intensity')
-    plt.title('Daily NOVA Stock Advice')
-    plt.grid()
-    plt.savefig('./NOVA_Stock_Advice.png')
-
-    sleep(3600 - time() % 3600)
-    main()
-
-
-if __name__ == "__main__":
-    main()
+keys = list(output_dict.keys())
+vals = [float(output_dict[k]['BUY']) for k in keys]
+plt.style.use('seaborn-dark')
+plt.figure(figsize=(18, 5))
+ax = sns.barplot(x=keys, y=vals)
+now = datetime.now()
+time_string = now.strftime("%d/%m/%Y %H:%M:%S")
+plt.text(0.005, 0.02, f"Generated at {time_string} - Garv Shah", fontsize=14, transform=plt.gcf().transFigure)
+plt.xlabel('\nSymbol')
+plt.ylabel('Buy Intensity')
+plt.title('Daily NOVA Stock Advice')
+plt.grid()
+plt.savefig('./NOVA_Stock_Advice.png')
